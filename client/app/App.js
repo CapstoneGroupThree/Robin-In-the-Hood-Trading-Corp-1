8a269658f526333689c64ce84bc0a16f04e07965
Hanlynui <hanyu.lin59@gmail.com>
2023-05-26 19:20:11 -0400

    added is mobile check, react native app is in development

--- client/app/App.js
+++ client/app/App.js
@@ -6,6 +6,23 @@ import AppRoutes from "./AppRoutes";
 
 const App = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const isMobile = window.innerWidth <= 768;
+
+  if (isMobile) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            React Native App is not done yet
+          </h1>
+          <p className="text-gray-600">
+            Please visit the site on your desktop.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       {isLoggedIn ? (
